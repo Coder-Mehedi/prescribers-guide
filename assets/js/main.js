5c89ce31ff0f1470e2ab6f20e1bf7b1c45f44ff2
Coder-Mehedi <mehedi5051@gmail.com>
2019-07-21 14:20:43 +0600

    font family and margin padding changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
 $(() => {
-    $(window).scroll(() => {
-        if ($(window).scrollTop() > 83) {
-            $('.main-menu').addClass('fixed-menu');
-        } else {
-            $('.main-menu').removeClass('fixed-menu');
-        }
+    $('button.other').on('click', () => {
+        $('#other').slideToggle();
+    })
+    $('button.cinv').on('click', () => {
+        $('#cinv').slideToggle();
+    })
+    $('button.anxiety').on('click', () => {
+        $('#anxiety').slideToggle();
     })
 })
